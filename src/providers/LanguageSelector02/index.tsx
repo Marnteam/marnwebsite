978a7f3741f471da10e4ef75469eb3614a7a2ad9
agi-v0 <omaroubari16@gmail.com>
2025-09-09 23:29:28 +0300

    ui improvements

--- src/providers/LanguageSelector02/index.tsx
+++ src/providers/LanguageSelector02/index.tsx
@@ -1,13 +1,11 @@
 'use client'
 
-import { cn } from '@/utilities/ui'
-import { motion } from 'motion/react'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useRouter } from 'next/navigation'
+import { Button } from '@/components/ui/button'
 
 export const LanguageSwitcher: React.FC<{ locale: string }> = ({ locale }) => {
   const router = useRouter()
-  const [value, setValue] = useState(locale) // Set initial value to the current locale
 
   const languages = [
     { code: 'ar', label: 'العربية', display: 'ع' },
@@ -24,52 +22,39 @@ export const LanguageSwitcher: React.FC<{ locale: string }> = ({ locale }) => {
       .split('/')
       .filter((segment) => segment && segment !== locale)
 
-    // Construct the new path
     const newPath = `/${newLocale}${pathSegments.length > 0 ? `/${pathSegments.join('/')}` : ''}`
-
     router.push(newPath)
   }
 
-  useEffect(() => {
-    setValue(locale) // Ensure the selected value stays updated when the locale changes
-  }, [locale])
+  const code = locale === 'en' ? 'ar' : 'en'
+  const language = languages.find((lang) => lang.code !== locale) || languages[0]
 
   return (
     <div className="flex flex-row items-center gap-2">
-      <p className="text-base-tertiary text-sm font-medium">
+      {/* <p className="text-base-tertiary text-sm font-medium">
         {locale === 'ar' ? 'اللغة' : 'Language'}
-      </p>
-      <div className={cn('relative flex h-auto rounded-full border p-1')}>
-        {languages.map(({ code, label, display }) => {
-          const isActive = value === code
-
-          return (
-            <button
-              type="button"
-              key={code}
-              className="relative w-6 rounded-full" // Added px-2 for better text spacing
-              onClick={() => onLanguageChange(code)}
-              aria-label={label}
-            >
-              {isActive && (
-                <motion.div
-                  layoutId="activeLanguage" // Unique layoutId for language switcher
-                  className="bg-background absolute inset-0 rounded-full"
-                  transition={{ type: 'spring', duration: 0.5 }}
-                />
-              )}
-              <span // Using span for text
-                className={cn(
-                  'relative m-auto text-xs leading-0', // Adjusted text size
-                  isActive ? 'text-base-primary' : 'text-base-secondary', // Adjusted colors for better contrast
-                )}
-              >
-                {display}
-              </span>
-            </button>
-          )
-        })}
-      </div>
+      </p> */}
+      <Button
+        type="button"
+        color="neutral"
+        variant="tertiary"
+        size="md"
+        key={code}
+        className="relative rounded-full text-sm" // Added px-2 for better text spacing
+        onClick={() => onLanguageChange(code)}
+        aria-label={language.label}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="20px"
+          viewBox="0 -960 960 960"
+          width="20px"
+          fill="currentColor"
+        >
+          <path d="M480-96q-79 0-149-30t-122.5-82.5Q156-261 126-331T96-480q0-80 30-149.5t82.5-122Q261-804 331-834t149-30q80 0 149.5 30t122 82.5Q804-699 834-629.5T864-480q0 79-30 149t-82.5 122.5Q699-156 629.5-126T480-96Zm0-75q17-17 34-63.5T540-336H420q9 55 26 101.5t34 63.5Zm-91-10q-14-30-24.5-69T347-336H204q29 57 77 97.5T389-181Zm182 0q60-17 108-57.5t77-97.5H613q-7 47-17.5 86T571-181ZM177-408h161q-2-19-2.5-37.5T335-482q0-18 .5-35.5T338-552H177q-5 19-7 36.5t-2 35.5q0 18 2 35.5t7 36.5Zm234 0h138q2-20 2.5-37.5t.5-34.5q0-17-.5-35t-2.5-37H411q-2 19-2.5 37t-.5 35q0 17 .5 35t2.5 37Zm211 0h161q5-19 7-36.5t2-35.5q0-18-2-36t-7-36H622q2 19 2.5 37.5t.5 36.5q0 18-.5 35.5T622-408Zm-9-216h143q-29-57-77-97.5T571-779q14 30 24.5 69t17.5 86Zm-193 0h120q-9-55-26-101.5T480-789q-17 17-34 63.5T420-624Zm-216 0h143q7-47 17.5-86t24.5-69q-60 17-108 57.5T204-624Z" />
+        </svg>
+        {language.label}
+      </Button>
     </div>
   )
 }
--- src/providers/Theme/ThemeSelector/index.tsx
+++ src/providers/Theme/ThemeSelector/index.tsx
@@ -3,31 +3,63 @@
 import React, { useState } from 'react'
 
 import { cn } from '@/utilities/ui'
-import { Monitor, Moon, Sun } from 'lucide-react'
-import { motion } from 'motion/react'
-
-const themes = [
-  {
-    key: 'auto',
-    icon: Monitor,
-    label: 'System theme',
-  },
-  {
-    key: 'light',
-    icon: Sun,
-    label: 'Light theme',
-  },
-  {
-    key: 'dark',
-    icon: Moon,
-    label: 'Dark theme',
-  },
-]
-
+import { AnimatePresence, motion } from 'motion/react'
 import type { Theme } from './types'
 
 import { useTheme } from '..'
 import { themeLocalStorageKey } from './types'
+import { Button } from '@/components/ui/button'
+
+const themes = [
+  {
+    key: 'auto',
+    icon: ({ className }: { className?: string }) => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={className}
+        viewBox="0 -960 960 960"
+        height="20px"
+        width="20px"
+        fill="currentColor"
+      >
+        <path d="M384-240H168q-29.7 0-50.85-21.15Q96-282.3 96-312v-433q0-29.7 21.15-50.85Q138.3-817 168-817h624q29.7 0 50.85 21.15Q864-774.7 864-745v433q0 29.7-21.15 50.85Q821.7-240 792-240H576l40.89 40.89Q618-198 624-182v14.25q0 9.5-7.2 16.62Q609.6-144 600-144H353q-7.29 0-12.14-4.82-4.86-4.83-4.86-12.06V-185q0-3 5-12l43-43ZM168-432h624v-313H168v313Zm0 0v-313 313Z" />
+      </svg>
+    ),
+    label: 'System theme',
+  },
+  {
+    key: 'light',
+    icon: ({ className }: { className?: string }) => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={className}
+        viewBox="0 -960 960 960"
+        height="20px"
+        width="20px"
+        fill="currentColor"
+      >
+        <path d="M444-828v-48q0-15.3 10.29-25.65Q464.58-912 479.79-912t25.71 10.35Q516-891.3 516-876v48q0 15.3-10.29 25.65Q495.42-792 480.21-792t-25.71-10.35Q444-812.7 444-828Zm0 744v-48q0-15.3 10.29-25.65Q464.58-168 479.79-168t25.71 10.35Q516-147.3 516-132v48q0 15.3-10.29 25.65Q495.42-48 480.21-48T454.5-58.35Q444-68.7 444-84Zm432-360h-48q-15.3 0-25.65-10.29Q792-464.58 792-479.79t10.35-25.71Q812.7-516 828-516h48q15.3 0 25.65 10.29Q912-495.42 912-480.21t-10.35 25.71Q891.3-444 876-444Zm-744 0H84q-15.3 0-25.65-10.29Q48-464.58 48-479.79t10.35-25.71Q68.7-516 84-516h48q15.3 0 25.65 10.29Q168-495.42 168-480.21t-10.35 25.71Q147.3-444 132-444Zm653-288-34 32q-11 11-25.5 10.5T700-701q-11-11-11-25t11-25l33-35q11-11 26.51-11 15.51 0 26.45 11.17 11.04 11.17 10.54 27T785-732ZM260-209l-32 34q-11 11-26.51 11-15.51 0-26.45-11.17-11.04-11.17-10.54-27T176-229l33-31q11-11 25.5-10.5T260-259q11 11 11 25t-11 25Zm472 33-32-33q-11-11-10.5-25.5T701-260q11-11 25-11.5t25 10.5l35 33q11 11 11 26.51 0 15.51-11.17 26.45-11.17 11.04-27 10.54T732-176ZM209-700l-34-33q-11-11-11.5-26t10.5-26q11-11 27.5-11.5T229-785l32 34q10 11 9.5 25.5T259-700q-11 11-25 11t-25-11Zm271 460q-100 0-170-70t-70-170q0-100 70-170t170-70q100 0 170 70t70 170q0 100-70 170t-170 70Zm-.25-71Q550-311 599-360.5t49-119.75q0-70.24-48.75-119-48.76-48.75-119-48.75Q410-648 361-599.25q-49 48.76-49 119 0 70.25 48.75 119.75 48.76 49.5 119 49.5ZM481-481Z" />
+      </svg>
+    ),
+    label: 'Light theme',
+  },
+  {
+    key: 'dark',
+    icon: ({ className }: { className?: string }) => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={className}
+        viewBox="0 -960 960 960"
+        height="20px"
+        width="20px"
+        fill="currentColor"
+      >
+        <path d="M483-96q-80.47 0-150.87-30.65-70.41-30.66-122.62-82.86-52.2-52.21-82.86-122.62Q96-402.53 96-483q0-120.58 67.5-218.79T343-844q13-5 25.5-4t21.94 9q7.56 7 11.56 18.5 4 11.5 4 28.5 0 78 29 147.5T521-518q54.32 54.07 124.16 83.03Q715-406 792-406q17 0 28.5 4t18.5 11.56q8 9.44 9.5 21.94 1.5 12.5-3.21 25.2Q802-231 702.8-163.5 603.6-96 483-96Zm0-72q88 0 164-45t115-122q-83-5-158.5-39.5T469-468q-60-60-94-135t-40-159q-77 41-122 116.18-45 75.19-45 162.82 0 131.25 91.88 223.12Q351.75-168 483-168Zm-14-300Zm106-181-70-69q-11-10.91-11-25.45Q494-758 505-769l70-70q10.91-11 25.45-11Q615-850 626-839l69 70q11 10.91 11 25.45Q706-729 695-718l-69 69q-10.91 11-25.45 11Q586-638 575-649Zm191.95 95.25-21.3-21.5Q735-586 735-600.4q0-14.4 10.75-25.2l21.5-21.6Q778-658 792-658t25 10.8l22 21.6q11 10.8 11 25.2 0 14.4-11 25.15l-22 21.5Q806-543 791.8-543t-24.85-10.75Z" />
+      </svg>
+    ),
+    label: 'Dark theme',
+  },
+]
 
 export const ThemeSelector: React.FC<{ locale: string }> = ({ locale }) => {
   const { setTheme } = useTheme()
@@ -50,35 +82,39 @@ export const ThemeSelector: React.FC<{ locale: string }> = ({ locale }) => {
 
   return (
     <div className="flex flex-row items-center gap-2">
-      <p className="text-base-tertiary text-sm font-medium">
+      {/* <p className="text-base-tertiary text-sm font-medium">
         {locale === 'ar' ? 'المظهر' : 'Theme'}
-      </p>
-      <div className={cn('relative flex h-auto rounded-full border p-1')}>
+      </p> */}
+      <div className="relative flex gap-1">
         {themes.map(({ key, icon: Icon, label }) => {
           const isActive = value === key
 
           return (
-            <button
+            <Button
+              variant="tertiary"
+              size="icon"
               type="button"
               key={key}
-              className="relative rounded-full p-1"
+              className={cn(
+                'group text-base-tertiary hover:text-base-primary relative rounded-full p-1',
+                { 'text-base-primary': isActive },
+              )}
               onClick={() => onThemeChange(key as Theme & 'auto')}
               aria-label={label}
             >
-              {isActive && (
-                <motion.div
-                  layoutId="activeTheme"
-                  className="bg-background absolute inset-0 rounded-full"
-                  transition={{ type: 'spring', duration: 0.5 }}
-                />
-              )}
-              <Icon
-                className={cn(
-                  'relative m-auto size-4',
-                  isActive ? 'text-base-secondary' : 'text-base-tertiary',
+              <AnimatePresence>
+                {isActive && (
+                  <motion.div
+                    className="bg-background-neutral border-border absolute inset-0 z-0 rounded-full border"
+                    initial={{ scale: '0%' }}
+                    animate={{ scale: '100%' }}
+                    exit={{ scale: '0%' }}
+                    transition={{ type: 'spring', duration: 0.3 }}
+                  />
                 )}
-              />
-            </button>
+              </AnimatePresence>
+              <Icon className="relative z-1 m-auto size-5" />
+            </Button>
           )
         })}
       </div>
