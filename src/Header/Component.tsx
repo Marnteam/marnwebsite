096535dd7198748ee26970a4054d2e24bb275355
agi-v0 <omaroubari16@gmail.com>
2025-10-27 00:24:01 +0300

    update getGlobals()

--- src/Header/Component.tsx
+++ src/Header/Component.tsx
@@ -4,8 +4,10 @@ import React from 'react'
 
 import type { Header } from '@/payload-types'
 import type { PayloadAdminBarProps } from 'payload-admin-bar'
+import { getLocale } from 'next-intl/server'
 
 export async function Header({ adminBarProps }: { adminBarProps: PayloadAdminBarProps }) {
-  const headerData = (await getCachedGlobal('header', 2)()) as Header
+  const locale = await getLocale()
+  const headerData = (await getCachedGlobal('header', locale, 2)()) as Header
   return <HeaderClient {...headerData} adminBarProps={adminBarProps} />
 }
