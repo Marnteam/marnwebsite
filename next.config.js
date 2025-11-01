aab661cda4b82470e7ebd354936958abcbce2ab0
agi-v0 <omaroubari16@gmail.com>
2025-11-01 22:51:54 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,7 +17,6 @@ console.log('env: ', process.env.NODE_ENV)
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
         const url = new URL(item)
