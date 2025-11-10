5921ac042a9c59cc3c2f2e830a3f8e4a81d62047
agi-v0 <omaroubari16@gmail.com>
2025-11-10 13:19:48 +0300

    disable focalPoint

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -129,7 +129,7 @@ export const Media: CollectionConfig = {
     // Upload to the public/media directory in Next.js making them publicly accessible even outside of Payload
     staticDir: path.resolve(dirname, '../../public/media'),
     // adminThumbnail: 'thumbnail',
-    focalPoint: true,
+    // focalPoint: true,
     // imageSizes: [
     //   {
     //     name: 'thumbnail',
