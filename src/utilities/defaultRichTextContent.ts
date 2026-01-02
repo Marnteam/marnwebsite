0ced8cda8e501aaadcfa03a0508d15efad67c7bf
agi-v0 <omaroubari16@gmail.com>
2026-01-03 00:07:36 +0300

    fix

--- src/utilities/defaultRichTextContent.ts
+++ src/utilities/defaultRichTextContent.ts
@@ -2,7 +2,12 @@ import { SerializedRootNode } from '@payloadcms/richtext-lexical/lexical'
 
 export const EMPTY_CONTENT: { root: SerializedRootNode } = {
   root: {
-    children: [],
+    children: [
+      {
+        type: 'paragraph',
+        version: 1,
+      },
+    ],
     direction: null,
     format: '',
     indent: 0,
